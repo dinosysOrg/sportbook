28358c224aac8d83c072405c373b244903e6c338
Duy Nguyen <di.dy211@gmail.com>
2017-04-10 20:50:47 +0700

    add rspec for auth

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +21,22 @@ module Backend
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '**', '*')]
+
+    config.i18n.available_locales = :vi
+    config.i18n.default_locale = :vi
+
+    config.action_controller.allow_forgery_protection = false
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                 methods: [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
