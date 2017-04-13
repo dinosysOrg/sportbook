55e4030c67062aa8b660b2ad56c689c84555e79c
Zi <hungryzi@gmail.com>
2017-04-13 18:11:15 +0700

    Commented out devise validations for now

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
-  include DeviseTokenAuth::Concerns::User
+  # devise :database_authenticatable, :registerable,
+  #         :recoverable, :rememberable, :trackable, :validatable,
+  #         :confirmable
+  # include DeviseTokenAuth::Concerns::User
 end
