f7dbb9a15c7b135d1a9a8cbb2ba2021cd7d1d51d
nhatdev1193 <nhat.nguyen@dinosys.vn>
2017-05-18 13:51:21 +0700

    Add exception handler

--- app/api/v1/exception_handlers.rb
+++ app/api/v1/exception_handlers.rb
@@ -1,60 +1,50 @@
 module V1
   module ExceptionHandlers
-    def handle_invalid_record!(e)
-      errors = e.record.errors.messages.map do |attr, messages|
-        messages.map { |message| { attribute: attr, message: message } }
-      end.flatten
-      error!(
-        { errors: errors, status_code: 422 },
-        422
-      )
-    end
+    extend ActiveSupport::Concern
 
-    def handle_record_not_found!(e)
-      errors = [
-        message: e.message,
-        model: e.model,
-        attribute: e.primary_key,
-        value: e.send(e.primary_key)
-      ]
-      error!(
-        { errors: errors, status_code: 404 },
-        404
-      )
-    end
-
-    def handle_validation_errors!(e)
-      errors = e.errors.map do |k, v|
-        { message: v.first, attribute: k.first }
+    included do
+      rescue_from ActiveRecord::RecordInvalid do |e|
+        errors = e.record.errors.messages.map do |attr, messages|
+          messages.map { |message| { attribute: attr, message: message } }
+        end.flatten
+        error!(
+          { errors: errors, status_code: 422 },
+          422
+        )
       end
-      error!(
-        { errors: errors, status_code: 422 },
-        422
-      )
-    end
 
-    def handle_unexpected_errors!(e)
-      raise e if Rails.env.development? || Rails.env.test?
-
-      error!(
-        { errors: [{ message: 'Unexpected error happened on server' }], status_code: 500 },
-        500
-      )
-      # TODO
-      # error notification: Rollbar...
-    end
-
-    def handle_authorization_errors!(_)
-      if current_api_user.nil?
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        errors = [
+          message: e.message,
+          model: e.model,
+          attribute: e.primary_key,
+          value: e.send(e.primary_key)
+        ]
         error!(
-          { errors: [{ message: 'Login required' }], status_code: 401 },
-          401
+          { errors: errors, status_code: 404 },
+          404
         )
-      else
+      end
+
+      rescue_from Grape::Exceptions::ValidationErrors do |e|
+        errors = e.errors.map do |k, v|
+          { message: v.first, attribute: k.first }
+        end
         error!(
-          { errors: [{ message: 'Forbidden' }], status_code: 403 },
-          403
+          { errors: errors, status_code: 422 },
+          422
         )
+      end
+
+      rescue_from :all do |e|
+        raise e if Rails.env.development? || Rails.env.test?
+
+        error!(
+          { errors: [{ message: 'Unexpected error happened on server' }], status_code: 500 },
+          500
+        )
+        # TODO
+        # error notification: Rollbar...
       end
     end
   end
--- app/api/v1/teams_api.rb
+++ app/api/v1/teams_api.rb
@@ -8,13 +8,15 @@ module V1
       authenticate_api_user!
     end
 
+    include ExceptionHandlers
+
     desc 'Sign up tournament'
     params do
       requires :name, type: String
     end
     post 'tournaments/:tournament_id/teams' do
       tour = Tournament.find_by_id(params[:tournament_id])
-      team = Team.create(name: params[:name], tournament_id: params[:tournament_id], status: :registered, venue_ranking: params[:venue_ranking])
+      team = Team.create!(name: params[:name], tournament_id: params[:tournament_id], status: :registered, venue_ranking: params[:venue_ranking])
 
       date_range = (tour['start_date']..tour['end_date']).to_a
       TimeSlotService.instance.create_from_preferred_time_blocks([team], date_range, params[:preferred_time_blocks])
