4d626e752785c7c8fe1e78853dc51a30cf70faf6
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2017-05-24 16:04:32 +0700

    update api matches

--- spec/api/v1/matches_api_spec.rb
+++ spec/api/v1/matches_api_spec.rb
@@ -1,6 +1,6 @@
 describe 'MatchesApi' do
   let!(:tour) { create(:tournament, start_date: 1.days.from_now, end_date: 2.weeks.from_now) }
-  let!(:user) {create(:tournament)}
+  let!(:user) { create(:tournament)}
   it 'Player can view their upcoming confirmed matches' do
     create(:team, id: 1, tournament_id: tour.id, name: "Thanh Tùng")
     create(:team, id: 2, tournament_id: tour.id, name: "Thái Duy")
@@ -8,15 +8,34 @@ describe 'MatchesApi' do
     create(:team, id: 4, tournament_id: tour.id, name: "Phương Thảo")
     create(:team, id: 5, tournament_id: tour.id, name: "Nguyen Hoang Lam")
     create(:team, id: 6, tournament_id: tour.id, name: "Nguyễn Nhật Thanh")
+    create(:team, id: 7, tournament_id: (tour.id + 1), name: "Nguyễn Nhật Thanh 2")
+    create(:team, id: 8, tournament_id: (tour.id + 1), name: "Nguyễn Nhật  Phat")
     create(:match, id: 1, team_a_id: 6, team_b_id: 1)
     create(:match, id: 2, team_a_id: 6, team_b_id: 2)
     create(:match, id: 3, team_a_id: 3, team_b_id: 6)
     create(:match, id: 4, team_a_id: 4, team_b_id: 5)
+    create(:match, id: 5, team_a_id: 7, team_b_id: 8)
     create(:user)
     get "/api/v1/matches", 
-    params: { user_id: nil , tournament_id: 4  }
+    params: { user_id: nil , tournament_id: tour.id+1  }
     expect(response.status).to eq(200)
-    expect(response.body)
+    assert response.ok?
+    if request.params["user_id"] == nil || request.params["user_id"] != nil
+      if request.params["tournament_id"] == nil
+        expect(JSON.parse(json_response).count).to eq(5)
+        puts "Case: tournament_id and user_id is nil"  
+        puts "The number of matches = 5"
+      end
+      if request.params["tournament_id"].to_i == tour.id
+        expect(JSON.parse(json_response).count).to eq(4)
+        puts "Case: tournament_id not nil"
+        puts "The number of matches = 4"
+      end
+      if request.params["tournament_id"].to_i == tour.id+1
+        expect(JSON.parse(json_response).count).to eq(1)
+        puts "Case: tournament_id not nil"
+        puts "The number of matches = 1"
+      end
+    end
   end
-
 end
