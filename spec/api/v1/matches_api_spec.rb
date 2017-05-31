d119a2b25e90c2a31bacf62c8c094e184283d8b5
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2017-05-31 10:53:13 +0700

    update matches_api_spec.rb

--- spec/api/v1/matches_api_spec.rb
+++ spec/api/v1/matches_api_spec.rb
@@ -1,21 +1,21 @@
 describe 'MatchesApi' do
-  let!(:user) { create(:user) }
+  let(:api_user) { create(:api_user) }
+  let!(:match) { create(:match) }
   context 'Player can view their upcoming confirmed matches' do
     it 'When not found user in matches' do
-      create_list(:match, 5)
-      get "/api/v1/user/#{user.id}/matches"
+      get "/api/v1/user/#{api_user.id}/matches"
       expect(response.status).to eq(200)
       expect(json_response[:_embedded][:matches].count).to eq(0)
     end
     it 'When found user in matches' do
       team_a = create(:team)
       team_b = create(:team)
-      api_user = create(:api_user)
       create(:player, team: team_a, user: api_user)
-      matches = create(:match, team_a: team_a, team_b: team_b)
+      match_a = create(:match, team_a: team_a, team_b: team_b)
       get "/api/v1/user/#{api_user.id}/matches"
       expect(response.status).to eq(200)
-      expect(json_response[:_embedded][:matches][0][:id]).to eq(matches.id)
+      expect(json_response[:_embedded][:matches].count).to eq(1)
+      expect(json_response[:_embedded][:matches][0][:id]).to eq(match_a.id)
     end
   end
 end
