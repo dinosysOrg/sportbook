705815d270c1108a75cb64dc445998dffc765117
hungryzi <hungryzi@gmail.com>
2017-06-02 17:43:50 +0700

    Renamed skills_api_spec

--- spec/api/v1/skills_api_spec.rb
+++ spec/api/v1/skills_api_spec.rb
@@ -2,6 +2,6 @@ describe 'SkillsAPI' do
   it 'show all skills' do
     get '/api/v1/skills'
     expect(response.status).to eq(200)
-    expect(json_response[:_embedded][:skills].count).to eq(5)
+    expect(json_response[:_embedded][:skills].count).to eq(Skill.count)
   end
 end
