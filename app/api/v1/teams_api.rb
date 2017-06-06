b79dac5614c0e2bcbe00b82b1ff1d75615343c52
Thang Hoang <thang.hoang@dinosys.vn>
2017-06-06 18:21:09 +0700

    fix build

--- app/api/v1/teams_api.rb
+++ app/api/v1/teams_api.rb
@@ -58,9 +58,7 @@ module V1
       team = Team.find_by_id params[:id]
       tour = team.tournament
       date_range = (tour['start_date']..tour['end_date']).to_a
-      team.time_slots.where(available: true).each do |time_slot|
-        time_slot(&:destroy)
-      end
+      team.time_slots.where(available: true).each(&:destroy)
       TimeSlotService.create_from_preferred_time_blocks([team], date_range, params[:preferred_time_blocks])
       present team, with: Representers::TeamRepresenter
     end
