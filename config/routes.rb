c233fc974198a4579b7ee60d00de4984e1da2806
tho vo <tho.vo@dinosys.edu.vn>
2017-07-05 14:35:00 +0700

    reset password for client

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, { class_name: 'User' }.merge(ActiveAdmin::Devise.config.merge(skip: [:registrations]))
+  devise_for :users, only: [:sessions, :passwords]
   ActiveAdmin.routes(self)
 
   mount ApplicationApi => '/'
